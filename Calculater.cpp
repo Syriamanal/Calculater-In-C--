19c25731faf369d35605784a351dee90a8aa5b90
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 15:59:44 -0800

    Update Calculater.cpp

--- Calculater.cpp
+++ Calculater.cpp
@@ -10,19 +10,19 @@ int Num2;
 int Duv;
 int Sum;
 
-cout << "Pleas enter a number...";
+cout << "Pleas enter a number...\n";
 cout << endl;
 cin >> Num1;
-cout << "Pleas enter a symbole Ex: +, *, - ";
+cout << "Pleas enter a symbole Ex: +, *, - \n";
 cin >> Duv;
 cout << endl;
-cout << "Pleas enter a number...";
+cout << "Pleas enter a number...\n";
 cin >> Num2;
 cout << endl;
 
 Sum = Num1, Duv, Num2;
 
-cout << "processing...";
+cout << "processing...\n";
 cout << endl;
 cout << Num1 << Duv << Num2 << " Is " << Sum << endl;
 return 0;
